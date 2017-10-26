7eb3231e1c9a86ae8382e5e9191d749e33f5bfff
Connor <connor.alcock.87@gmail.com>
2017-10-26 08:33:41 -0600

    Restarted front end structure after an error

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Vagabond Eats</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Use the map to navigate available food trucks
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
