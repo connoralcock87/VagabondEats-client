b6a1558a7e70403ce5bd849f0df33cc30be64156
Connor <connor.alcock.87@gmail.com>
2017-10-27 15:24:57 -0600

    Map component working with test data

---
+++ client/src/components/Map.js
@@ -0,0 +1,48 @@
+import React from 'react'
+// import ReactDOM from 'react-dom'
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
+
+const position = [39.742043, -104.991531]
+
+export class MapView extends React.Component {
+  // constructor(props) {
+  //   super(props)
+  // }
+  render() {
+
+    const cordsTest = [39.758265, -105.007315]
+    const cordsTest2 = [39.761981, -104.981562]
+    const cordsTest3 = [39.769584, -104.905980]
+
+    const key1 = 1
+
+    return (
+      <div>
+        <Map
+          style={{height: "70vh", width: "80vw"}}
+          center={position}
+          zoom={13}>
+          <TileLayer
+            url="https://api.mapbox.com/styles/v1/spacebear09/cj98oo7d51pp52so2i77m9ybi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BhY2ViZWFyMDkiLCJhIjoiY2o5NWczaTd6NGg1dDMzbzRmMnRtd2wyOCJ9.ajGxzHLCM4ABWnct4Fy3NQ"
+            attribution="<attribution>" />
+            <Marker key={key1} position={cordsTest}>
+             <Popup>
+               <span>Burger Chief<br/>1pm-9pm<br/>1695 Platte Street<br/></span>
+             </Popup>
+            </Marker>
+            <Marker key={key1} position={cordsTest2}>
+             <Popup>
+              <span>Chicken Rebel<br/>2927 Larimer St<br/>5pm-10pm<br/></span>
+            </Popup>
+            </Marker>
+            <Marker key={key1} position={cordsTest3}>
+             <Popup>
+              <span>Rocky Mountain Slices<br/>7045 E 38th Ave<br/> <br/>2pm-9pm<br/></span>
+             </Popup>
+           </Marker>
+         </Map>
+      </div>
+    )
+  }
+}
+export default MapView
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import MapView from './components/Map.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Vagabond Eats</h1>
         </header>
-        <MapView />
+        <p className="App-intro">
+          Use the map to navigate available food trucks
+        </p>
       </div>
     );
   }
